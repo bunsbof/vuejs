45c2034478f340e0e60c406258e714d12926b6b8
The <nguyenminhcuong2000@gmail.com>
2022-08-27 00:02:35 +0700

    end of this net-ninja project :)))

--- net-ninja/src/views/HomeView.vue
+++ net-ninja/src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
+import { ref } from 'vue';
+
+const searchQuery = ref("")
 
 </script>
 
 <template>
-    <main>
-        
-    </main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        v-model="searchQuery"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+    </div>
+  </main>
 </template>
