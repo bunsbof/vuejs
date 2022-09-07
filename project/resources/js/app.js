8ebe0a1627a3858b354f2b69530bd6c049bb3d8a
bunsbof <nguyenminhcuong2000@gmail.com>
2022-09-07 12:27:53 +0700

    #2022/09/07 cuong - failed

--- project/resources/js/app.js
+++ project/resources/js/app.js
@@ -1,1 +1,23 @@
-require('./bootstrap');
+import './bootstrap';
+import Vue from 'vue/dist/vue'
+window.Vue = require('vue')
+
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import { routes } from './routes';
+
+Vue.useAttrs(VueRouter)
+Vue.useAttrs(VueAxios, axios)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+})
+
+const app = new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App),
+})
